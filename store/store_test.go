0b182e6c9aeeaa61b8f30a1e10c62dbf51c5ac82
Yanke Guo <ryan@islandzero.net>
2017-09-19 00:24:24 +0800

    add test for SetJSTicket

--- store/store_test.go
+++ store/store_test.go
@@ -7,13 +7,28 @@ import (
 	"ireul.com/redis"
 )
 
-func TestStore(t *testing.T) {
+func TestStoreAccessToken(t *testing.T) {
 	s := NewStore(redis.NewClient(&redis.Options{Addr: "localhost:6379"}))
 	err := s.SetAccessToken("hello", "a", time.Second*10)
 	if err != nil {
 		t.Error(err)
 	}
 	k, err := s.GetAccessToken("hello")
+	if err != nil {
+		t.Error(err)
+	}
+	if k != "a" {
+		t.Error("not equals " + k + "!=" + "a")
+	}
+}
+
+func TestStoreJSTicket(t *testing.T) {
+	s := NewStore(redis.NewClient(&redis.Options{Addr: "localhost:6379"}))
+	err := s.SetJSTicket("hello", "a", time.Second*10)
+	if err != nil {
+		t.Error(err)
+	}
+	k, err := s.GetJSTicket("hello")
 	if err != nil {
 		t.Error(err)
 	}
