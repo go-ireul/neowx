af52b4f880e4366f4110ca4488dbafe140c49f57
Yanke Guo <ryan@islandzero.net>
2017-09-18 16:11:06 +0800

    remove cdata tag

--- types/message.go
+++ types/message.go
@@ -6,37 +6,37 @@ import "encoding/xml"
 type Message struct {
 	XMLName xml.Name `xml:"xml"`
 	// Basic Fields, not empty
-	ToUserName   string `xml:",cdata"`
-	FromUserName string `xml:",cdata"`
+	ToUserName   string
+	FromUserName string
 	CreateTime   string
-	MsgType      string `xml:",cdata"`
+	MsgType      string
 	// Message
 	MsgID string `xml:"MsgId,omitempty" json:"MsgId,omitempty"`
 	// TextMessage
-	Content string `xml:",cdata,omitempty" json:",omitempty"`
+	Content string `xml:",omitempty" json:",omitempty"`
 	// RichMessage
-	MediaID string `xml:"MediaId,cdata,omitempty" json:"MediaId,omitempty"`
+	MediaID string `xml:"MediaId,omitempty" json:"MediaId,omitempty"`
 	// ImageMessage
-	PicURL string `xml:"PicUrl,cdata,omitempty" json:"PicUrl,omitempty"`
+	PicURL string `xml:"PicUrl,omitempty" json:"PicUrl,omitempty"`
 	// VoiceMessage
-	Format      string `xml:",cdata,omitempty" json:",omitempty"`
-	Recognition string `xml:",cdata,omitempty" json:",omitempty"`
+	Format      string `xml:",omitempty" json:",omitempty"`
+	Recognition string `xml:",omitempty" json:",omitempty"`
 	// VideoMessage // ShortVideoMessage
-	ThumbMediaID string `xml:"ThumbMediaId,cdata,omitempty" json:"ThumbMediaId,omitempty"`
+	ThumbMediaID string `xml:"ThumbMediaId,omitempty" json:"ThumbMediaId,omitempty"`
 	// LocationMessage
 	LocationX string `xml:"Location_X,omitempty" json:"Location_X,omitempty"`
 	LocationY string `xml:"Location_Y,omitempty" json:"Location_Y,omitempty"`
 	Scale     string `xml:",omitempty" json:",omitempty"`
-	Label     string `xml:",cdata,omitempty" json:",omitempty"`
+	Label     string `xml:",omitempty" json:",omitempty"`
 	// LinkMessage
-	Title       string `xml:",cdata,omitempty" json:",omitempty"`
-	Description string `xml:",cdata,omitempty" json:",omitempty"`
-	URL         string `xml:"Url,cdata,omitempty" json:"Url,omitempty"`
+	Title       string `xml:",omitempty" json:",omitempty"`
+	Description string `xml:",omitempty" json:",omitempty"`
+	URL         string `xml:"Url,omitempty" json:"Url,omitempty"`
 	// Event
-	Event    string `xml:",cdata,omitempty" json:",omitempty"`
-	EventKey string `xml:",cdata,omitempty" json:",omitempty"`
+	Event    string `xml:",omitempty" json:",omitempty"`
+	EventKey string `xml:",omitempty" json:",omitempty"`
 	// SubscribeEvent
-	Ticket string `xml:",cdata,omitempty" json:",omitempty"`
+	Ticket string `xml:",omitempty" json:",omitempty"`
 	// LocationEvent
 	Latitude  string `xml:",omitempty" json:",omitempty"`
 	Longitude string `xml:",omitempty" json:",omitempty"`
